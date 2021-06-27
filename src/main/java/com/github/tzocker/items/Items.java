e1079e9bb768cfcba5f1ce18258eeb3659e21889
T-Zocker <tzoockeer@gmail.com>
2021-06-27 23:35:39 +0200

    Added lithium nuggets

--- src/main/java/com/github/tzocker/items/Items.java
+++ src/main/java/com/github/tzocker/items/Items.java
@@ -8,6 +8,7 @@ import net.minecraft.util.registry.Registry;
 public class Items {
     public static final Item LITHIUM_INGOT = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
     public static final Item RAW_LITHIUM= new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static final Item LITHIUM_NUGGET = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
 
     public static void register(String name, Item item) {
         Registry.register(Registry.ITEM, new Identifier("lithium", name), item);
@@ -16,5 +17,6 @@ public class Items {
     public static void init() {
         register("lithium_ingot", LITHIUM_INGOT);
         register("raw_lithium", RAW_LITHIUM);
+        register("lithium_ingot", LITHIUM_NUGGET);
     }
 }
