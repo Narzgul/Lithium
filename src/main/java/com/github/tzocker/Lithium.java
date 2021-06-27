6b67d4fd3bc2865e9817499701dfdcdd5e0845cc
T-Zocker <tzoockeer@gmail.com>
2021-06-27 22:42:26 +0200

    Ore generation for Lithium

--- src/main/java/com/github/tzocker/Lithium.java
+++ src/main/java/com/github/tzocker/Lithium.java
@@ -11,5 +11,6 @@ public class Lithium implements ModInitializer {
 
 		Items.init();
 		Blocks.init();
+		OreGen.init();
 	}
 }
---
+++ src/main/java/com/github/tzocker/OreGen.java
@@ -0,0 +1,37 @@
+package com.github.tzocker;
+
+import com.github.tzocker.blocks.Blocks;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.heightprovider.UniformHeightProvider;
+
+public class OreGen {
+    private static final ConfiguredFeature<?, ?> LITHIUM_ORE_OVERWORLD = Feature.ORE
+            .configure(new OreFeatureConfig(
+                    OreFeatureConfig.Rules.BASE_STONE_OVERWORLD,
+                    Blocks.LITHIUM_ORE.getDefaultState(),
+                    9)) // Vein size
+            .range(new RangeDecoratorConfig(
+                    // You can also use one of the other height providers if you don't want a uniform distribution
+                    UniformHeightProvider.create(YOffset.aboveBottom(0), YOffset.fixed(63)))) // Inclusive min and max height
+            .spreadHorizontally()
+            .repeat(20); // Number of veins per chunk
+
+    public static void init() {
+        RegistryKey<ConfiguredFeature<?, ?>> lithiumOreOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
+                new Identifier("lithium", "lithium_ore_overworld"));
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, lithiumOreOverworld.getValue(), LITHIUM_ORE_OVERWORLD);
+        //noinspection deprecation
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, lithiumOreOverworld);
+    }
+}
