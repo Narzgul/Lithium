bf7e5e9f1dd9ceaad28c6320478071291d8ce340
T-Zocker <tzoockeer@gmail.com>
2021-06-27 15:09:42 +0200

    Added a first item

--- src/main/java/com/github/tzocker/Lithium.java
+++ src/main/java/com/github/tzocker/Lithium.java
@@ -1,14 +1,13 @@
 package com.github.tzocker;
 
+import com.github.tzocker.common.Items;
 import net.fabricmc.api.ModInitializer;
 
 public class Lithium implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		System.out.println("Starting Lithium...");
 
-		System.out.println("Hello Fabric world!");
+		Items.init();
 	}
 }
---
+++ src/main/java/com/github/tzocker/common/Items.java
@@ -0,0 +1,18 @@
+package com.github.tzocker.common;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class Items {
+    public static final Item LITHIUM_INGOT = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+
+    public static void register(String name, Item item) {
+        Registry.register(Registry.ITEM, new Identifier("lithium", name), item);
+    }
+
+    public static void init() {
+        register("lithium_ingot", LITHIUM_INGOT);
+    }
+}
